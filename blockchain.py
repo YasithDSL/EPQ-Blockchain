1e88d2dc34dc455c513c2241033a9f17e9a858bd
Yasith <42208881+YasithDSL@users.noreply.github.com>
2021-08-09 20:36:33 +0100

    implementing chain - phase 1

--- blockchain.py
+++ blockchain.py
@@ -22,8 +22,8 @@ class Blockchain(object):
         }
 
         self.current_transactions = []
-
         self.chain.append(block)
+
         return block
 
     def new_transactions(self, sender, recipient, amount):
@@ -35,10 +35,26 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
     @staticmethod
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
+
         return hashlib.sha256(block_string).hexdigest()
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
 
     @property
     def last_block(self):
